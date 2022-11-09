bec8eca04a235c61e0dde22edc38870fb731255a
Sergey Suyagin <sergey.a.suyagin@gmail.com>
2022-11-09 14:34:20 +0400

    Some more small adjustments and improvements for reviewers

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert 'login' in self.browser.current_url, 'No \"login\" in current url'
+        assert 'login' in self.browser.current_url, 'No login in current url'
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), 'No login form on login page'
